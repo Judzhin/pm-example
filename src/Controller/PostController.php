2f72cb18ac5e19c85512f94d87ec14b79db3ec8b
Judzhin Miles <info@msbios.com>
2019-11-24 21:08:13 +0200

    Intermediate commit

---
+++ src/Controller/PostController.php
@@ -0,0 +1,120 @@
+<?php
+/**
+ * @access protected
+ * @author Judzhin Miles <info[woof-woof]msbios.com>
+ */
+namespace App\Controller;
+
+use App\Entity\Post;
+use App\Form\PostType;
+use App\Repository\PostRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * Class PostController
+ * @package App\Controller
+ *
+ * @Route("/post")
+ */
+class PostController extends AbstractController
+{
+    /**
+     * @Route("/", name="pm_posts", methods={"GET"})
+     *
+     * @param PostRepository $postRepository
+     * @return Response
+     */
+    public function index(PostRepository $postRepository): Response
+    {
+        return $this->render('post/index.html.twig', [
+            'posts' => $postRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="pm_post_new", methods={"GET","POST"})
+     *
+     * @param Request $request
+     * @return Response
+     */
+    public function new(Request $request): Response
+    {
+        /** @var Post $post */
+        $post = new Post;
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($post);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('pm_posts');
+        }
+
+        return $this->render('post/new.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="pm_post_show", methods={"GET"})
+     *
+     * @param Post $post
+     * @return Response
+     */
+    public function show(Post $post): Response
+    {
+        return $this->render('post/show.html.twig', [
+            'post' => $post,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="pm_post_edit", methods={"GET","POST"})
+     *
+     * @param Request $request
+     * @param Post $post
+     * @return Response
+     */
+    public function edit(Request $request, Post $post): Response
+    {
+        /** @var FormInterface $form */
+        $form = $this->createForm(PostType::class, $post);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('pm_posts');
+        }
+
+        return $this->render('post/edit.html.twig', [
+            'post' => $post,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="pm_post_delete", methods={"DELETE"})
+     *
+     * @param Request $request
+     * @param Post $post
+     * @return Response
+     */
+    public function delete(Request $request, Post $post): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$post->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($post);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('pm_posts');
+    }
+}
